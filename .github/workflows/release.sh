54029524cf38ed24b4ea422177e3c5250a8db4a2
Jayesh Bapu Ahire <jayeshbahire@programmer.net>
2021-01-12 09:30:05 +0530

    test

--- .github/workflows/release.sh
+++ .github/workflows/release.sh
@@ -1,4 +1,4 @@
 echo 'dependencies:'
 echo '  -   name: kafka'
 echo '      repository: "https://storage.googleapis.com/hypertrace-helm-charts"'
-echo "      version: ${{ steps.kafka.outputs.release }}"
+echo "      version: $kafka_version"
