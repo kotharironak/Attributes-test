f2f41557c59a897a6d325390f68ae5d089124bd2
Jayesh Bapu Ahire <jayeshbahire@programmer.net>
2021-01-12 13:55:28 +0530

    test

---
+++ .github/workflows/commit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global user.email "jayeshbahire@gmail.com"
+git config --global user.name "JBAhire"
+git add . 
+git commit -m "Updated Repository data"
+git push origin main
