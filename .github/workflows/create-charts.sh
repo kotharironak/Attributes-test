b4c18ac95fec45681a025e57c2baf2dce4a733e2
Jayesh Bapu Ahire <jayeshbahire@programmer.net>
2021-01-12 13:39:00 +0530

    test

--- .github/workflows/create-charts.sh
+++ .github/workflows/create-charts.sh
@@ -2,7 +2,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 OUT_DIR="kubernetes/data-services"
 
 print_header() {
-    cat "${SCRIPTDIR}/header.txt"
+    cat "header.txt"
     echo
 }
 
